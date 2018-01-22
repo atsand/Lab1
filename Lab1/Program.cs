b827944687d3c1eca9989db6feafd6694b1d836d
Aaron Sandusky <atsand23@gmail.com>
2018-01-22 18:43:04 -0500

    first commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side1, side2, height;
+            bool cont = true;
+
+            while (cont)
+            {
+                string again;
+                Console.WriteLine("Welcome to the room calculator.");
+                Console.WriteLine("Please enter the lenth of one side of the room in decimal form.");
+                side1 = (double.Parse(Console.ReadLine()));
+                Console.WriteLine("Please enter the lenth of the adjacent side of the room in decimal form.");
+                side2 = (double.Parse(Console.ReadLine()));
+                Console.WriteLine("Please enter the height of the room in decimal form.");
+                height = (double.Parse(Console.ReadLine()));
+                Console.WriteLine("The area of the room is {0}.", (side1 * side2));
+                Console.WriteLine("The perimeter of the room is {0}.", ((2 * side1) + (2 * side2)));
+                Console.WriteLine("The volume of the room is {0}.", (side1*side2*height));
+                Console.WriteLine("Would you like to try again? (Y/N)");
+                again = Console.ReadLine();
+                if (again == "y" || again == "Y")
+                {
+                    cont = true;
+                }
+                else
+                {
+                    cont = false;
+                }
+            }
+        }
+    }
+}

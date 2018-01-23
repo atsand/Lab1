b616b1f28e66714f4b797f76570722e01ed11c19
Aaron Sandusky <atsand23@gmail.com>
2018-01-23 10:37:18 -0500

    second try

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab1
 {
@@ -19,15 +15,20 @@ namespace Lab1
                 Console.WriteLine("Welcome to the room calculator.");
                 Console.WriteLine("Please enter the lenth of one side of the room in decimal form.");
                 side1 = (double.Parse(Console.ReadLine()));
+
                 Console.WriteLine("Please enter the lenth of the adjacent side of the room in decimal form.");
                 side2 = (double.Parse(Console.ReadLine()));
+
                 Console.WriteLine("Please enter the height of the room in decimal form.");
                 height = (double.Parse(Console.ReadLine()));
+
                 Console.WriteLine("The area of the room is {0}.", (side1 * side2));
                 Console.WriteLine("The perimeter of the room is {0}.", ((2 * side1) + (2 * side2)));
                 Console.WriteLine("The volume of the room is {0}.", (side1*side2*height));
+
                 Console.WriteLine("Would you like to try again? (Y/N)");
                 again = Console.ReadLine();
+
                 if (again == "y" || again == "Y")
                 {
                     cont = true;
